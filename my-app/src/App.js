5f2e5865d1a2508259bd63d2f96c3414635a13e9
BaileyLuu <50964948+BaileyLuu@users.noreply.github.com>
2021-06-29 23:40:45 -0400

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Blahaj Gallery!</h1>
+      <img src="https://www.ikea.com/ca/en/images/products/blahaj-soft-toy-shark__0710175_pe727378_s5.jpg?f=s" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://i.ebayimg.com/images/g/nsAAAOSwdv9fbmh5/s-l300.jpg" alt="Girl in a jacket" ></img>
+      <br></br>
+      <img src="https://pbs.twimg.com/profile_images/1037395958806532097/l9tvEUxB_400x400.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://i.redd.it/i4jq1zd3h3s31.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://media.techeblog.com/images/ikea-blahaj-plush-shark.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://preview.redd.it/yr8dcze1gl321.jpg?auto=webp&s=040360036174f9afa2ac17b3abcb4ff362319e14" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://i.pinimg.com/originals/5b/1b/76/5b1b761f345312b7f28650802d2febb5.png" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://tlgrm.eu/_/stickers/8d6/f82/8d6f8279-7c1c-3184-aa35-6a5edb3919d3/1.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://i.pinimg.com/originals/3c/98/c5/3c98c5ff5e5f1139054740ff2d0bb0d1.png" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://i.pinimg.com/originals/a0/88/cc/a088cc9bfd8a2b3bdab3d2faf753c5aa.png" alt="Girl in a jacket" width="500" height="600"></img>
+
+      <br></br>
+      <img src="https://tlgrm.eu/_/stickers/8d6/f82/8d6f8279-7c1c-3184-aa35-6a5edb3919d3/3.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+
+      <br></br>
+      <img src="https://external-preview.redd.it/fMjo7UGrpzG-qfEimQ-YPnbdCvEdwADTiKfJ_u1KbYc.jpg?auto=webp&s=0aa0f033aecb501cf01f29351014337bc0cd3e3f" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://64.media.tumblr.com/3f857b7b942ef939d921862ddfb3bf34/2e9e67a981613a0d-0f/s640x960/6519a7076f7be5a101c5f48b296152de4e8fe59c.png" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/243203b0-8bd7-4a59-a2ba-525b1f289da3/dcoraq2-2a270bc7-d470-44d7-a5be-b9fca53c542a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI0MzIwM2IwLThiZDctNGE1OS1hMmJhLTUyNWIxZjI4OWRhM1wvZGNvcmFxMi0yYTI3MGJjNy1kNDcwLTQ0ZDctYTViZS1iOWZjYTUzYzU0MmEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3yOSchlqaiUnmfG8_jbtn-BEpdKL-hJ1cpbNtzkEdEo" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+      <img src="https://blahajgang.lol/assets/just-blahaj.png" alt="Girl in a jacket" width="500" height="600"></img>
+      <br></br>
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */
+
+
+      }
+    </div>
+  );
+}
+
+export default App;
